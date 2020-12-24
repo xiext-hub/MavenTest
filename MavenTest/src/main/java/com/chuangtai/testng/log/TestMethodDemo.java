6606bce18235d6e8c43a9a04ee0c72a4eeda8876
xiext <1075219353@qq.com>
2020-12-24 17:58:23 +0800

    Mock框架学ä¹记录

---
+++ MavenTest/src/main/java/com/chuangtai/testng/log/TestMethodDemo.java
@@ -0,0 +1,27 @@
+package com.chuangtai.testng.log;
+
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+
+//断言和log的使用
+public class TestMethodDemo {
+    @Test
+    public  void test1(){
+        Reporter.log("我是婷子噢");
+        Assert.assertEquals(1,2);
+    }
+
+    @Test
+    public  void test2(){
+
+        Assert.assertEquals("test","test");
+    }
+
+    @Test
+    public  void logDemo(){
+        Reporter.log("这是我写的日志哦");
+      //  throw new RuntimeException("这是我自己写的抛出的异常噢。");
+    }
+}
